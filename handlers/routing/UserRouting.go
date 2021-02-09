dfa1e10d8c7a490bcf7a10cf94793f83e30bf73f
Denis <Denis>
2021-02-09 20:16:59 +0100

    moved routing from main into separate files

---
+++ handlers/routing/UserRouting.go
@@ -0,0 +1,25 @@
+package routing
+
+
+import (
+	"net/http"
+	"github.com/DenisKnez/simpleWebGolang/handlers"
+)
+
+//UserRouting setup user routes
+func UserRouting (sm *http.ServeMux, userHandler *handlers.UserHandler) {
+
+	// GET /users
+	sm.HandleFunc("/users", userHandler.GetUsers)
+	// GET /users/id?id=
+	sm.HandleFunc("/users/id", userHandler.GetUser)
+	// POST /users/createuser
+	sm.HandleFunc("/users/createuser", userHandler.CreateUser)
+	// GET /users/paged?pagesize={1}&pagenumber={2}
+	sm.HandleFunc("/users/paged", userHandler.PagedUsers)
+	// DELETE /users/delete/id?id={1}
+	sm.HandleFunc("/users/delete/id", userHandler.DeleteUser)
+	// PUT /users/update/id?id={1}
+	sm.HandleFunc("/users/update/id", userHandler.UpdateUser)
+}
+
