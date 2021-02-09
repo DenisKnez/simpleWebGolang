d67d814783d20c609e9c7df2e724f08cac418c2d
Denis <Denis>
2021-02-09 20:18:03 +0100

    moving dependency injection into separate packages cause of circural dependencies

---
+++ diUtils/utilDependencyInjection.go
@@ -0,0 +1,22 @@
+package diutils
+
+import (
+	"log"
+	"os"
+
+	setup "github.com/DenisKnez/simpleWebGolang/util/setuputil"
+	"github.com/spf13/viper"
+)
+
+var config = setup.SetupConfig()
+var file, logger = setup.SetupLogger(config)
+
+//GetLogger get logger and the log file
+func GetLogger() (file *os.File, log *log.Logger) {
+	return file, logger
+}
+
+//GetConfig get config file
+func GetConfig() *viper.Viper {
+	return config
+}
