8f6f263d4372aef4eaab0706c5e4e23adecc542c
Denis <Denis>
2021-02-17 13:21:44 +0100

    added auth middleware to user routes and added useCase methods to dependency injection

--- diServices/serivceDependencyInjection.go
+++ diServices/serivceDependencyInjection.go
@@ -6,16 +6,28 @@ import (
 	"github.com/DenisKnez/simpleWebGolang/repositories"
 	"github.com/DenisKnez/simpleWebGolang/usecase"
 	"github.com/DenisKnez/simpleWebGolang/util"
+	"github.com/DenisKnez/simpleWebGolang/domains"
 )
 
+
+//usecase
+var AuthUseCase *domains.AuthUseCase
+var userUseCase *domains.UserUseCase
+var bookUseCase *domains.BookUseCase
+var publisherUseCase *domains.PublisherUseCase
+
+
+//handlers
+var authHandler *handlers.AuthHandler
 var userHandler *handlers.UserHandler
 var bookHandler *handlers.BookHandler
 var publisherHandler *handlers.PublisherHandler
 
+
 func init() {
 
 	_, logger := diutils.GetLogger()
-	//config := diutils.GetConfig()
+	config := diutils.GetConfig()
 
 	// user DI
 	userRepo := repositories.NewUserRepository(util.Db, logger)
@@ -31,6 +43,11 @@ func init() {
 	publisherRepo := repositories.NewPublisherRepository(util.Db, logger)
 	publisherUseCase := usecase.NewPublisherUseCase(publisherRepo, logger)
 	publisherHandler = handlers.NewPublisherHandler(publisherUseCase)
+
+	// auth DI
+	authRepo := repositories.NewAuthRepository(util.Db, logger)
+	authUseCase := usecase.NewAuthUseCase(authRepo, logger)
+	authHandler = handlers.NewAuthHandler(authUseCase, config)
 }
 
 //GetUserHandler Returns the user handler
@@ -46,4 +63,9 @@ func GetBookHandler() *handlers.BookHandler {
 //GetPublisherHandler Returns the publisher handler
 func GetPublisherHandler() *handlers.PublisherHandler {
 	return publisherHandler
+}
+
+//GetAuthHandler Returns the auth handler
+func GetAuthHandler() *handlers.AuthHandler {
+	return authHandler
 }
--- handlers/routing/UserRouting.go
+++ handlers/routing/UserRouting.go
@@ -3,24 +3,33 @@ package routing
 import (
 	"net/http"
 
+	"github.com/DenisKnez/simpleWebGolang/diservices"
+	"github.com/DenisKnez/simpleWebGolang/diutils"
 	"github.com/DenisKnez/simpleWebGolang/handlers"
 	"github.com/DenisKnez/simpleWebGolang/util/middleware"
 )
 
 //UserRouting setup user routes
-func UserRouting (sm *http.ServeMux, userHandler *handlers.UserHandler) {
+func UserRouting(sm *http.ServeMux, userHandler *handlers.UserHandler) {
+
+	config := diutils.GetConfig()
 
 	// GET /users
-	sm.HandleFunc("/users", middleware.Adapt(userHandler.GetUsers, middleware.NotifyMiddleware()))
+	sm.HandleFunc("/users", middleware.Adapt(userHandler.GetUsers,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 	// GET /users/id?id=
-	sm.HandleFunc("/users/id", userHandler.GetUser)
+	sm.HandleFunc("/users/id", middleware.Adapt(userHandler.GetUser,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 	// POST /users/createuser
-	sm.HandleFunc("/users/createuser", userHandler.CreateUser)
+	sm.HandleFunc("/users/createuser", middleware.Adapt(userHandler.CreateUser,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 	// GET /users/paged?pagesize={1}&pagenumber={2}
-	sm.HandleFunc("/users/paged", userHandler.PagedUsers)
+	sm.HandleFunc("/users/paged", middleware.Adapt(userHandler.PagedUsers,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 	// DELETE /users/delete/id?id={1}
-	sm.HandleFunc("/users/delete/id", userHandler.DeleteUser)
+	sm.HandleFunc("/users/delete/id", middleware.Adapt(userHandler.DeleteUser,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 	// PUT /users/update/id?id={1}
-	sm.HandleFunc("/users/update/id", userHandler.UpdateUser)
+	sm.HandleFunc("/users/update/id", middleware.Adapt(userHandler.UpdateUser,
+		middleware.AuthMiddleware(diservices.AuthUseCase, config)))
 }
-
